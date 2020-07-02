d98b0d8ca26471d61d0dd8fb42b6e4f1d36aaea1
Gois <matheusgoislimasilva@gmail.com>
2020-07-02 14:25:33 -0300

    Initial Commit

---
+++ Art4All/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Art4All
+//
+//  Created by Matheus Silva on 02/07/20.
+//  Copyright © 2020 Matheus Gois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
